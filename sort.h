64e1c6c208c94771fa471c3e2e1b9798bfb7b0cd
BKaoukine <b.koukin@gmail.com>
2023-12-15 21:56:53 +0100

    0. Bubble sort

---
+++ sort.h
@@ -0,0 +1,24 @@
+#ifndef SORT
+#define SORT
+
+
+/*Imported Libraries*/
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*Data Structures*/
+
+
+
+
+
+/*Functions Prototypes*/
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
+
+
+
+
+#endif /*End of SORT*/
